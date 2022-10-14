7c4dbbe3dd1c7a348232cc66950c0816d1a033bc
Ilya <ilja.kachalin@yandex.ru>
2022-10-14 22:01:38 +0300

    Task5

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом
+
+Console.Clear();
+
+Console.Write("Введите число ");
+string text = Convert.ToString(Console.ReadLine());
+
+int length = text.Length;
+for (int i = 0; i<length-1; i++)
+{
+if (text[i]==text[length-i]) Console.WriteLine("палиндром");
+else Console.WriteLine("Не палиндром");
+}
+
+ Console.WriteLine("Введите целое число");
+
+
